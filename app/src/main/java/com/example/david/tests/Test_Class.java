8dc1584001bc62ba8615f8e509d9382cf1c82a9a
Anton-20002 <0891872659azXY>
2018-03-31 15:13:39 +0300

    New One

---
+++ app/src/main/java/com/example/david/tests/Test_Class.java
@@ -0,0 +1,11 @@
+package com.example.david.tests;
+
+/**
+ * Created by Anton on 31.03.2018.
+ */
+
+public class Test_Class {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
